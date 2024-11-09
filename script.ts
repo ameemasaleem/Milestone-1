14c41d924452121569dc37863dfa049df63ee99d
Ameema Saleem <161148265+ameemasaleem@users.noreply.github.com>
2024-11-09 16:05:57 +0500

    initial project

---
+++ script.ts
@@ -0,0 +1,11 @@
+const toggleButton1 = document.getElementById('toggle-skills') as HTMLButtonElement
+const skills1 = document.getElementById('skills') as HTMLElement
+
+toggleButton1.addEventListener('click', () => {
+  if(skills1.style.display === 'none') {
+    skills1.style.display = 'block'
+  } else {
+        skills1.style.display = 'none'
+    }
+  }
+);
